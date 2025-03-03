3c1e989872f85f5f5602f7f4ac03acfc02ab4f14
sebas0505r <sebria26@bergen.org>
2025-03-03 10:22:08 -0500

    whitenoise-updated pt3

--- btre/settings.py
+++ btre/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'btre.urls'
